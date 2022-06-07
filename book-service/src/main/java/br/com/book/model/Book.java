67fcd87c7b53f1fc5709e7892ce35ac2de0ed526
Vítor <celiovitor62@gmail.com>
2022-06-06 21:23:39 -0300

    Atualizando projeto

--- book-service/src/main/java/br/com/book/model/Book.java
+++ book-service/src/main/java/br/com/book/model/Book.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "book")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
